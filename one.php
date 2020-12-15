887f59336eae737f7fd66d7dde69cdbd470df920
xulingxao <51937461+xulingxao@users.noreply.github.com>
2020-12-15 14:35:28 +0800

    Update one.php

--- one.php
+++ one.php
@@ -23,24 +23,105 @@ public function one()
             ['id' => 20, 'name' => '门徒20', 'master_id' => 18],
         ];
 
+        //根据父节点，给数组重新排序。
+        $array3 = $this->getTree($arr,0,0,true);
+
+
+        // 结果：
+         array:20 [▼
+          0 => array:4 [▼
+            "id" => 1
+            "name" => "大师"
+            "master_id" => 0
+            "level" => 0
+          ]
+          1 => array:4 [▼
+            "id" => 2
+            "name" => "剑术大弟子"
+            "master_id" => 1
+            "level" => 1
+          ]
+          2 => array:4 [▼
+            "id" => 6
+            "name" => "门徒6"
+            "master_id" => 2
+            "level" => 2
+          ]
+          3 => array:4 [▶]
+          4 => array:4 [▶]
+          5 => array:4 [▶]
+          6 => array:4 [▶]
+          7 => array:4 [▶]
+          8 => array:4 [▶]
+          9 => array:4 [▶]
+          10 => array:4 [▶]
+          11 => array:4 [▶]
+          12 => array:4 [▶]
+          13 => array:4 [▶]
+          14 => array:4 [▶]
+          15 => array:4 [▶]
+          16 => array:4 [▶]
+          17 => array:4 [▶]
+          18 => array:4 [▶]
+          19 => array:4 [▶]
+
         $arr2 = array();
+        //找到父节点为大师的节点 也就是master_id=1的节点(找到所有大弟子)
         foreach ($arr as $value){
             if ($value['master_id'] == 1){
+                //找到大弟子下的所有子节点，并将大弟子的id作为索引值
                 $arr2[$value['id']] = $this->getTree($arr,$value['id'],0,true);
             }
         }
-        $array3 = $this->getTree($arr,0,0,true);
+       // 结果：
+        array:3 [▼
+          2 => array:6 [▼
+            0 => array:4 [▶]
+            1 => array:4 [▶]
+            2 => array:4 [▶]
+            3 => array:4 [▶]
+            4 => array:4 [▶]
+            5 => array:4 [▶]
+          ]
+          3 => array:7 [▼
+            0 => array:4 [▶]
+            1 => array:4 [▶]
+            2 => array:4 [▶]
+            3 => array:4 [▶]
+            4 => array:4 [▶]
+            5 => array:4 [▶]
+            6 => array:4 [▶]
+          ]
+          4 => array:3 [▼
+            0 => array:4 [▶]
+            1 => array:4 [▶]
+            2 => array:4 [▶]
+          ]
+
+
+
+        //将弟子数量与大弟子对应
         foreach ($array3 as $key => $value){
+            //若id与在arr2中找到索引，拼接字符串。
             if (isset($arr2[$value['id']])){
                 $array3[$key]['name'] = $value['name']."(".count($arr2[$value['id']]).")";
             }
         }
+
+        //根据层级，将字符串‘--’重复。
         foreach ($array3 as $value) {
             echo str_repeat('--', $value['level']), $value['name'] . '<br />';
         }
     }
     
-  
+
+
+    /**
+     * $array 数组数据
+     * $pid 根节点
+     * $level 层级
+     * $status 是否需要数组初始化
+     **/
     function getTree($array, $pid = 0, $level = 0,$status = false)
     {
 
